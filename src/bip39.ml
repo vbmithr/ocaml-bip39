2abcd88949dae48e6ba089bcdae8f3a7658b46ec
Vincent Bernardoff <vb@luminar.eu.org>
2017-11-04 11:43:58 +0100

    add license

--- src/bip39.ml
+++ src/bip39.ml
@@ -1,3 +1,8 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2017 Vincent Bernardoff. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+  ---------------------------------------------------------------------------*)
+
 open StdLabels
 open Nocrypto
 
@@ -137,3 +142,19 @@ let to_seed ?(passphrase="") ({ lang ; indices } as t) =
   let salt = Cstruct.of_string ("mnemonic" ^ passphrase) in
   Pbkdf.pbkdf2 ~prf:`SHA512 ~password ~salt ~count:2048 ~dk_len:64l |>
   Cstruct.to_string
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2017 Vincent Bernardoff
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
